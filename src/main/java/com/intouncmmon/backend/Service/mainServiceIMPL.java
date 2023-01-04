844aad3ce8a0c0ee3583f8b1fc649d6b214e13b2
Subhath Abeysekara <abeysekarasdp.19@uom.lk>
2023-01-04 23:40:11 +0530

    add feedback date

--- src/main/java/com/intouncmmon/backend/Service/mainServiceIMPL.java
+++ src/main/java/com/intouncmmon/backend/Service/mainServiceIMPL.java
@@ -12,8 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.sql.Time;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.Timer;
 
 @Service
 public class mainServiceIMPL implements mainService{
@@ -112,6 +115,8 @@ public class mainServiceIMPL implements mainService{
     public String addFeedback(feedback feedback) {
 
         try {
+            Date date = new Date();
+            feedback.setDate(date);
             feedbackRepo.save(feedback);
             return "success";
         }
